f1a0d4c096f979e15e6eef267bb0f759ac333b92
Indiana Jonez <indiana.jonez@yandex.ru>
2023-04-21 21:46:05 +0300

    create and add table

--- MyHabits/HabitsViewController.swift
+++ MyHabits/HabitsViewController.swift
@@ -9,8 +9,55 @@ import UIKit
 
 class HabitsViewController: UIViewController {
     
+    private lazy var tableHabits: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.delegate = self
+        table.dataSource = self
+        return table
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray6
+        layout()
+    }
+    
+    
+    private func layout() {
+        view.addSubview(tableHabits)
+        
+        NSLayoutConstraint.activate([
+            tableHabits.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableHabits.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableHabits.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableHabits.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
     }
 }
+
+
+// MARK: - UITableViewDelegate
+
+extension HabitsViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableViewDataSource
+
+extension HabitsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        4
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+
+}
+
+
+
+
